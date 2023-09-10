a84571b868d04a2f7e5145c5a70c134851e6780c
Christopher Warren <chrisalmith@gmail.com>
2023-09-10 14:20:36 -0400

    add dynamic score styling

--- src/components/sections/ScoreSection.jsx
+++ src/components/sections/ScoreSection.jsx
@@ -1,13 +1,29 @@
 import './ScoreSection.css';
 
 const ScoreSection = ({ score }) => {
+  const calcScore = (score) => {
+    console.log;
+    let value = '';
+    if (score >= 80) {
+      value = 'Great';
+    } else if (score >= 60) {
+      value = 'Good';
+    } else {
+      value = 'Low';
+    }
+    return value;
+  };
+
   return (
     <div className="score-container">
-      <div className="result">
+      <div className={`result ${calcScore(score)}`}>
         <p>{score}%</p>
       </div>
       <p>
-        Overall Cyber Insurability Score: <span>{score}% - Low</span>
+        Overall Cyber Insurability Score:{' '}
+        <span>
+          {score}% - {calcScore(score)}
+        </span>
       </p>
     </div>
   );
